6a427fd6e66c36695104bbc181bb2a888d12b049
Techsup-IT <75061824+Techsup-IT@users.noreply.github.com>
2021-04-26 10:17:40 +0300

    Delete controller/frontend directory

--- controller/frontend/change_lang_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(isset($_POST['site_lang']) && $_POST['site_lang'] != ''){
-	$_SESSION['site_lang'] = $_POST['site_lang'];
-	die();
-}
-
-?>
--- controller/frontend/url_controller.php
+++
@@ -1,3 +0,0 @@
-<?php
-$url = $_SESSION['company_id'] . '___' . str_replace('_', '', SITE_URL) . '/';
-$url_encrpt = $security->encrypt($url, key);
